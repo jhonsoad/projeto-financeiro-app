3b8837518b8e11aea44a94184167518a393faab3
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-08-01 21:19:26 -0300

    cria componentes do dashboard

---
+++ src/app/components/dashboard/statement-card/statement-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-statement-card',
+  imports: [],
+  templateUrl: './statement-card.component.html',
+  styleUrl: './statement-card.component.css'
+})
+export class StatementCardComponent {
+
+}
---
+++ src/app/pages/dashboard/transferencias/transferencias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transferencias',
+  imports: [],
+  templateUrl: './transferencias.component.html',
+  styleUrl: './transferencias.component.css'
+})
+export class TransferenciasComponent {
+
+}
