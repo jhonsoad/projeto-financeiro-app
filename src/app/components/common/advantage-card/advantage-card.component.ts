4f9372ae2520fbb89c7d8a759d30b6c273047789
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-08-01 21:18:57 -0300

    cria componentes comuns da aplicacao

---
+++ src/app/components/common/advantage-card/advantage-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advantage-card',
+  imports: [],
+  templateUrl: './advantage-card.component.html',
+  styleUrl: './advantage-card.component.css'
+})
+export class AdvantageCardComponent {
+
+}
