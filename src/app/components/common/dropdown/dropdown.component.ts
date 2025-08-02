30f5d382afabeadf54e32a421ead36acbe2748b5
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-08-01 21:55:15 -0300

    cria template e regras dos componentes comuns

--- src/app/components/common/dropdown/dropdown.component.ts
+++ src/app/components/common/dropdown/dropdown.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+
+export interface DropdownOption {
+  value: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-dropdown',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.css'
 })
 export class DropdownComponent {
+  @Input() label?: string;
+  @Input() options: DropdownOption[] = [];
+  @Input() id?: string;
+  @Input() className: string = '';
+  // Outras props do HTML, como 'disabled', 'name', etc., podem ser passadas via Input
+  @Input() name?: string;
+  @Input() disabled: boolean = false;
 
+  get selectId(): string | undefined {
+    return this.id || (this.label ? this.label.toLowerCase().replace(/\s/g, '-') : undefined);
+  }
 }
--- src/app/components/common/input/input.component.ts
+++ src/app/components/common/input/input.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
+  @Input() label?: string;
+  @Input() id?: string;
+  @Input() className: string = '';
+  // Outras props do HTML, como 'type', 'placeholder', etc., podem ser passadas
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
 
+  get inputId(): string | undefined {
+    return this.id || (this.label ? this.label.toLowerCase().replace(/\s/g, '-') : undefined);
+  }
 }
