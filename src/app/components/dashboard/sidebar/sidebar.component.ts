ed5560c166af3d262979c951e7cd59d4c47c0cf8
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-08-03 17:19:36 -0300

    chama sidebar na page de inicio

---
+++ src/app/components/dashboard/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class SidebarComponent {
+  @Input() activeLink: string = 'Inicio';
+}
--- src/app/pages/dashboard/inicio/inicio.component.ts
+++ src/app/pages/dashboard/inicio/inicio.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { GetSaldoService } from '../../../services/get-saldo-conta/get-saldo-conta.service';
 import { loadMoviments } from '../../../shared/store/transaction.actions';
 import { Movement } from '../../../shared/interfaces/finance.interface';
 import { selectMovimentacoes } from '../../../shared/store/transaction.selectors';
+import { SidebarComponent } from '../../../components/dashboard/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule, SidebarComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
 
     constructor(
     private store: Store,
