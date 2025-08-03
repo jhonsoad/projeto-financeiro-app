2f2fc8c4ee64960961ed355a94fe95d418360fc9
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2025-08-03 17:04:57 -0300

    instala e configura o ngrx no projeto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideStore } from '@ngrx/store'; // Importe o provideStore
 import { routes } from './app.routes';
+import { transactionReducer } from './shared/store/transaction.reducer';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    // Adicione a configuração do NgRx aqui
+    provideStore({
+      transacao: transactionReducer
+    })
+  ]
 };
--- src/app/pages/dashboard/inicio/inicio.component.ts
+++ src/app/pages/dashboard/inicio/inicio.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GetSaldoService } from '../../../services/get-saldo-conta/get-saldo-conta.service';
+import { loadMoviments } from '../../../shared/store/transaction.actions';
+import { Movement } from '../../../shared/interfaces/finance.interface';
+import { selectMovimentacoes } from '../../../shared/store/transaction.selectors';
 
 @Component({
   selector: 'app-inicio',
@@ -7,5 +12,20 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+
+    constructor(
+    private store: Store,
+    private saldoService: GetSaldoService
+  ) { }
+
+  ngOnInit(): void {
+    this.saldoService.getSaldoTotal().subscribe((movements: Movement[]) => {
+      this.store.dispatch(loadMoviments({ movements }));
+    })
+
+    this.store.select(selectMovimentacoes).subscribe(movimentacoesDoEstado => {
+      console.log('Movimentações no estado do NgRx:', movimentacoesDoEstado);
+    });
+  }
 
 }
